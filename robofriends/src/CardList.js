c1fc23c08fae4a0eb18395a61b580a5fe7b4030f
Derek Adams <derekadams.uas@gmail.com>
2019-12-26 12:40:21 -0700

    added all robots with map loop

---
+++ robofriends/src/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+    const cardComponent = robots.map((user, index) => {
+        return <Card id={robots[index].id} name={robots[index].name} email={robots[index].email} />
+    })
+    return (
+        <div>
+            {cardComponent}
+        </div>
+    );
+}
+
+export default CardList;
