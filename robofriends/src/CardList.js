decac147e1e1b6a7b92b71e7c41426ae98e1d303
Derek Adams <derekadams.uas@gmail.com>
2019-12-26 13:43:54 -0700

    added search field to filter robot cards

--- robofriends/src/CardList.js
+++ robofriends/src/CardList.js
@@ -3,12 +3,20 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, index) => {
-        return <Card id={robots[index].id} name={robots[index].name} email={robots[index].email} />
-    })
     return (
         <div>
-            {cardComponent}
+            {
+                robots.map((user, index) => {
+                    return (
+                        <Card
+                            key={index}
+                            id={robots[index].id}
+                            name={robots[index].name}
+                            email={robots[index].email}
+                        />
+                    );
+                })
+            }
         </div>
     );
 }
