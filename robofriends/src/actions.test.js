1ca76e448e9fa8a245bd8b7902d3f0acd95b0e30
Derek Adams <derekadams.uas@gmail.com>
2020-01-07 22:21:05 -0700

    cleaned up actions test file

--- robofriends/src/actions.test.js
+++ robofriends/src/actions.test.js
@@ -1,9 +1,7 @@
 import * as actions from './actions';
 import {
     CHANGE_SEARCH_FIELD,
-    REQUEST_ROBOTS_PENDING,
-    REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAILED
+    REQUEST_ROBOTS_PENDING
 } from './constants.js'
 
 import configureMockStore from 'redux-mock-store';
